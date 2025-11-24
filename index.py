02cb8f8b7afde9ffc78fc27b3de05e648cc39d74
mtapiero12 <147454434+mtapiero12@users.noreply.github.com>
2025-11-23 22:08:12 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
-import tkinter as tk
-from tkinter import messagebox
 # Autoria: Mario Tapiero
 # Programa: Ingenieria de Sistemas
 # Semestre: Quinto
 
+import tkinter as tk
+from tkinter import messagebox
 import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
